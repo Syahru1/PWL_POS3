dd76706062adf2c20312ca7b985a5b3f30981dc7
Muhammad Syahrul Gunawan <syahrulgunawann41@gmail.com>
2025-03-19 07:10:23 +0700

    Minggu 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -179,13 +179,20 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu sedang aktif
+        $level = LevelModel::all(); // ambil data level untuk filter level
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
     public function list(Request $request) {
-        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level')->get();
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        // Filter data user verdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+        
         return DataTables::of($users) 
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,6 +15,22 @@
         @if (session('error'))
           <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group row">
+              <label class="col-1 control-label col-form-label">Filter:</label>
+            <div class="col-3">
+              <select class="form-control" id="level_id" name="level_id" required>
+                <option value="">- Semua -</option>
+                @foreach ($level as $item)
+                  <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                @endforeach
+              </select>
+              <small class="form-text text-muted">Level Pengguna</small>
+            </div>
+            </div>
+          </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user"> 
           <thead> 
             <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr> 
@@ -25,6 +41,24 @@
 @endsection 
  
 @push('css') 
+  <script>
+    $(document).ready(function() {
+      var dataUser = $('#table_user').DataTable({
+        serverSide: true, // serverSide: true, jika ingin menggunakan seerver side processing
+        ajax: {
+          "url": "{{ url('user/list') }}",
+          "dataType": "json",
+          "type": "POST",
+          "data": function (d) {
+            d.level_id = $('#level_id').val();
+          }
+        },
+        columns: [arguments{
+          data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
+        }]
+      });
+    });
+  </script>
 @endpush 
  
 @push('js') 
@@ -51,6 +85,9 @@
             } 
           ] 
       }); 
+      $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
+      });
     }); 
   </script> 
 @endpush  
